2cf3e9712cc080935fe05b51d978e30488cdbe57
VadimKalayda <vadosgta1@gmail.com>
2019-10-09 19:57:07 +0300

    added a few conditions index.js

--- JS tasks/index.js
+++ JS tasks/index.js
@@ -1,9 +1,13 @@
-var myArray = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2,-3,-1];
+var myArray = [3,0,-5,1,44,null, undefined, 'hello',-12,3,0,0,1,2,-3,-3,2,1,4,-2,-3,-1];
 
 function sumArray(arr) {
   var sum = 0;
   for(i = 0; i < arr.length; i++) {
-    sum += arr[i];
+    if(typeof(arr[i]) === 'number') {
+      sum += arr[i];
+    } else {
+      sum = sum;
+    }
   }
   console.log('sum = ', sum);
 }
@@ -11,8 +15,12 @@ function sumArray(arr) {
 function maxArray(arr) {
   var myMax = 0;
   for(i = 0; i < arr.length; i++) {
-    if(arr[i] >= myMax) {
-      myMax = arr[i];
+    if(typeof(arr[i]) === 'number') {
+      if(arr[i] >= myMax) {
+        myMax = arr[i];
+      }
+    } else {
+      myMax = myMax;
     }
   }
   console.log('max = ', myMax)
@@ -21,8 +29,12 @@ function maxArray(arr) {
 function minArray(arr) {
   var myMin = 0;
   for(i = 0; i < arr.length; i++) {
-    if(arr[i] <= myMin) {
-      myMin = arr[i];
+    if(typeof(arr[i]) === 'number') {
+      if(arr[i] <= myMin) {
+        myMin = arr[i];
+      } else {
+        myMin = myMin;
+      }
     }
   }
   console.log('min = ', myMin)
